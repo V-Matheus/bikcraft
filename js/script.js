4e8b36c0359bd52696df0241d93cf8a0aff3d482
Victor Matheus <victormatheus507@gmail.com>
2023-03-24 14:50:35 -0300

    animações

--- js/script.js
+++ js/script.js
@@ -44,3 +44,26 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicletas-imagens img");
+const galeriaContainer = document.querySelector(".bicicletas-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if(media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+//Animação
+if(window.SimpleAnime) {
+  new SimpleAnime()
+}
